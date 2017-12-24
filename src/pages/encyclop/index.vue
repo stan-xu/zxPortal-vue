749e4dd114c1efab5bd2ad8f483d26bb1b824306
Root <940150534@qq.com>
2017-12-25 00:11:32 +0800

    add header selected

--- src/pages/encyclop/index.vue
+++ src/pages/encyclop/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="encyclop">
-    <my-header></my-header>
+    <my-header :selected="2"></my-header>
     <div class="container"><img src="./images/en-banner.jpg" class="img-responsive center-block banner"></div>
     <Encyclop-menu></Encyclop-menu>
     <Encyclop-content></Encyclop-content>
